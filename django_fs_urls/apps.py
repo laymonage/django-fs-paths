a0bd7ed4886a7f216626e92e9ca764840ad96986
Sage Abdullah <me@laymonage.com>
2022-05-03 19:14:11 +0700

    Format with black

--- django_fs_urls/apps.py
+++ django_fs_urls/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DjangoFsUrlsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_fs_urls'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_fs_urls"
--- tests/settings.py
+++ tests/settings.py
@@ -1,26 +1,26 @@
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-SECRET_KEY = 'django-insecure-^%&_xdy77h_o2bc!6c2=5hrsn)dkne=x=c*x=q8x(l%n=+b%53'
+SECRET_KEY = "django-insecure-^%&_xdy77h_o2bc!6c2=5hrsn)dkne=x=c*x=q8x(l%n=+b%53"
 DEBUG = True
 
 INSTALLED_APPS = [
-    'django_fs_urls',
-    'tests',
+    "django_fs_urls",
+    "tests",
 ]
 
-ROOT_URLCONF = 'django_fs_urls.urls'
+ROOT_URLCONF = "django_fs_urls.urls"
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
-LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'UTC'
+LANGUAGE_CODE = "en-us"
+TIME_ZONE = "UTC"
 USE_I18N = True
 USE_TZ = True
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
