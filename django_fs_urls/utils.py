35ba2c0ca397e0d0a617b9f7d59eb01c202d2d29
Sage Abdullah <me@laymonage.com>
2022-05-23 00:16:01 +0700

    Add docstring for make_dispatch

--- django_fs_urls/utils.py
+++ django_fs_urls/utils.py
@@ -2,6 +2,16 @@ from django.http import HttpResponse
 
 
 def make_dispatch(text):
+    """
+    A helper function to create a dispatch function that returns an
+    HttpResponse. Originally, I wanted to make it so that the text is
+    automatically inferred from the module path, but it needs some `inspect`
+    thing to work. So, just pass the __name__ as the text from the modules.
+
+    This is just to make testing easier and is not necessary for
+    django-fs-urls to work.
+    """
+
     def dispatch(request):
         return HttpResponse(text)
 
